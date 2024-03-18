6285ecfc312370c936306e76efc73c786ea016f0
Stackz9 <svini4456@gmail.com>
2024-03-18 12:20:14 -0300

    add mais músicas

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const image = document.getElementById('cover'),
-    title = document.getElementById('music-title'),
-    artist = document.getElementById('music-artist'),
-    currentTimeEl = document.getElementById('current-time'),
-    durationEl = document.getElementById('duration'),
-    progress = document.getElementById('progress'),
-    playerProgress = document.getElementById('player-progress'),
-    prevBtn = document.getElementById('prev'),
-    nextBtn = document.getElementById('next'),
-    playBtn = document.getElementById('play'),
-    background = document.getElementById('bg-img');
+const image = document.getElementById("cover"),
+  title = document.getElementById("music-title"),
+  artist = document.getElementById("music-artist"),
+  currentTimeEl = document.getElementById("current-time"),
+  durationEl = document.getElementById("duration"),
+  progress = document.getElementById("progress"),
+  playerProgress = document.getElementById("player-progress"),
+  prevBtn = document.getElementById("prev"),
+  nextBtn = document.getElementById("next"),
+  playBtn = document.getElementById("play"),
+  background = document.getElementById("bg-img")
 
-const music = new Audio();
+const music = new Audio()
 
 const songs = [
   {
@@ -49,78 +49,100 @@ const songs = [
     cover: "img/musica6.m4a",
     artist: "Jefferson & Suellen ",
   },
+  {
+    path: "img/imagem 7.jpg",
+    displayName: "Pode morar aqui",
+    cover: "img/musica7.mp3",
+    artist: "Theo Rubia",
+  },
+  {
+    path: "img/imagem 8.jpg",
+    displayName: "yeshua",
+    cover: "img/musica8.mp3",
+    artist: "José Augusto",
+  },
+  {
+    path: "img/imagem 9.jpg",
+    displayName: "1000 graus",
+    cover: "img/musica9.mp3",
+    artist: "Renascer Prise",
+  },
+  {
+    path: "img/imagem 10.jpg",
+    displayName: "Me ajude a melhorar (ao vivo)",
+    cover: "img/musica 10.mp3",
+    artist: "Eli Soares",
+  },
 ]
 
-let musicIndex = 0;
-let isPlaying = false;
+let musicIndex = 0
+let isPlaying = false
 
 function toggleplay() {
   if (isPlaying) {
-    pauseMusic();
+    pauseMusic()
   } else {
-    playMusic();
+    playMusic()
   }
 }
 
 function playMusic() {
-  isPlaying = true ;
+  isPlaying = true
   //change play button icon
-  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.classList.replace("fa-play", "fa-pause")
   //Set button hover title
-  playBtn.setAttribute("title", "Pause");
-  music.play();
+  playBtn.setAttribute("title", "Pause")
+  music.play()
 }
 
 function pauseMusic() {
-  isPlaying = false;
+  isPlaying = false
   //change pause button icon
-  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.classList.replace("fa-pause", "fa-play")
   //Set button hover title
-  playBtn.setAttribute("title", "Play");
-  music.pause();
+  playBtn.setAttribute("title", "Play")
+  music.pause()
 }
 
 function loadMusic(song) {
-  music.src = song.cover;
-  title.textContent = song.displayName;
-  artist.textContent = song.artist; // Correção feita aqui
-  image.src = song.path;
-  background.src = song.path ;// Ou background.src = song.cover; se preferir
-};
-
-
+  music.src = song.cover
+  title.textContent = song.displayName
+  artist.textContent = song.artist // Correção feita aqui
+  image.src = song.path
+  background.src = song.path // Ou background.src = song.cover; se preferir
+}
 
 function changeMusic(direction) {
-  musicIndex = (musicIndex + direction + songs.length) % songs.length;
-  loadMusic(songs[musicIndex]);
-  playMusic();
+  musicIndex = (musicIndex + direction + songs.length) % songs.length
+  loadMusic(songs[musicIndex])
+  playMusic()
 }
 
 function updateProgessBar() {
-  const { duration, currentTime } = music;
-  const progressPercent = (currentTime / duration) * 100;
-  progress.style.width = `${progressPercent}%`;
+  const { duration, currentTime } = music
+  const progressPercent = (currentTime / duration) * 100
+  progress.style.width = `${progressPercent}%`
 
-  const formatTime = (time) => String(Math.floor(time)).padStart(2, "0");
+  const formatTime = (time) => String(Math.floor(time)).padStart(2, "0")
   durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(
     duration % 60
-  )}`;
+  )}`
   currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(
     currentTime % 60
-  )}` ;
+  )}`
 }
 
 function setProgressBar(e) {
-  const width = playerProgress.clientWidth;
-  const clickX = e.offsetX;
-  music.currentTime = (clickX / width) * music.duration;
+  const width = playerProgress.clientWidth
+  const clickX = e.offsetX
+  music.currentTime = (clickX / width) * music.duration
 }
 
-playBtn.addEventListener("click", toggleplay);
-prevBtn.addEventListener("click", () => changeMusic(-1));
-nextBtn.addEventListener("click", () => changeMusic(1));
+playBtn.addEventListener("click", toggleplay)
+prevBtn.addEventListener("click", () => changeMusic(-1))
+nextBtn.addEventListener("click", () => changeMusic(1))
 music.addEventListener("ended", () => changeMusic(1))
-music.addEventListener("timeupdate", updateProgessBar);
-playerProgress.addEventListener("click", setProgressBar);
+music.addEventListener("timeupdate", updateProgessBar)
+playerProgress.addEventListener("click", setProgressBar)
 
-loadMusic(songs[musicIndex]);
+loadMusic(songs[musicIndex])
